79ad1f6e227b35d4f5e0500ff50db0a4417537c5
leonardopasqualotto <lpasqualotto@hotmail.com>
2024-09-12 23:45:46 -0300

    alteracoes para projeto busca cnpj

--- code/ETL_coletar_dados_e_gravar_BD.py
+++ code/ETL_coletar_dados_e_gravar_BD.py
@@ -80,7 +80,7 @@ if not os.path.isfile(dotenv_path):
 print(dotenv_path)
 load_dotenv(dotenv_path=dotenv_path)
 
-dados_rf = 'http://200.152.38.155/CNPJ/'
+dados_rf = 'http://200.152.38.155/CNPJ/dados_abertos_cnpj/2024-08/'
 
 #%%
 # Read details from ".env" file:
